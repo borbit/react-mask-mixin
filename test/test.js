3a977f9b9b401fbbde7a045c416489ba44067f01
Serge Borbit <serge.borbit@gmail.com>
2016-01-25 18:48:51 +0200

    Trying to fix tests.

--- test/test.js
+++ test/test.js
@@ -10,7 +10,11 @@ var InputComponent = React.createClass({
 var TestUtils = React.addons.TestUtils
 // shortcut
 var Input = function(props) {
-  return TestUtils.renderIntoDocument(React.createElement(InputComponent, props)).getDOMNode()
+  var body = document.getElementById('body')
+  var inst = React.createElement(InputComponent, props)
+  body.innerHTML = ''
+
+  return React.findDOMNode(React.render(inst, body))
 }
 
 describe('dummy component', function() {
@@ -69,25 +73,25 @@ describe('react-mask-mixin — mask behaviour with input', function() {
     TestUtils.Simulate.change(input, {target: {value: '11__--___'}})
     expect(input.value).to.be('11_-___')
   })
-  
+
   it('should replace mask with user input #2', function() {
     var input = Input({mask: '99[9-9]99'})
     TestUtils.Simulate.change(input, {target: {value: '11[1-1_]__'}})
     expect(input.value).to.be('11[1-1]__')
   })
-  
+
   it('should replace mask with user input #3', function() {
     var input = Input({mask: '999999'})
     TestUtils.Simulate.change(input, {target: {value: '11BB___'}})
     expect(input.value).to.be('11____')
   })
-  
+
   it('should replace mask with user input #4', function() {
     var input = Input({mask: '$AAAAA'})
     TestUtils.Simulate.change(input, {target: {value: '$FF000_'}})
     expect(input.value).to.be('$FF___')
   })
-  
+
   it('should replace mask with user input #5', function() {
     var input = Input({mask: '999-999'})
     TestUtils.Simulate.change(input, {target: {value: '111-1'}})
@@ -105,13 +109,13 @@ describe('react-mask-mixin — mask behaviour with input', function() {
     TestUtils.Simulate.change(input, {target: {value: '111___'}})
     expect(input.value).to.be('111')
   })
-  
+
   it('should replace mask with user input #8', function() {
     var input = Input({mask: '999-999'})
     TestUtils.Simulate.change(input, {target: {value: '111111'}})
     expect(input.value).to.be('111-111')
   })
-  
+
   it('should replace mask with user input #9', function() {
     var input = Input({mask: 'AAA-AAA'})
     TestUtils.Simulate.change(input, {target: {value: 'BBBB11'}})
@@ -216,14 +220,14 @@ describe('react-mask-mixin — coursor behaviour', function() {
   it('should move cursor to the end of user input #6', function() {
     var input = Input({mask: '999-999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(7, 7)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111-111'}})
 
     input.setSelectionRange(6, 6)
     TestUtils.Simulate.change(input, {target: {value: '111-11'}})
-    
+
     expect(input.selectionStart).to.be(6)
   })
 
@@ -240,21 +244,21 @@ describe('react-mask-mixin — coursor behaviour', function() {
   it('should move cursor to an available position if it is in the middle of user input #1', function() {
     var input = Input({mask: '999-999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(2, 2)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111-111'}})
-    
+
     input.setSelectionRange(4, 4)
     TestUtils.Simulate.change(input, {target: {value: '1112-111'}})
-    
+
     expect(input.selectionStart).to.be(5)
   })
 
   it('should move cursor to an available position if it is in the middle of user input #2', function() {
     var input = Input({mask: '999-999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(5, 5)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111-111'}})
@@ -268,84 +272,84 @@ describe('react-mask-mixin — coursor behaviour', function() {
   it('should move cursor to an available position if it is in the middle of user input #3', function() {
     var input = Input({mask: '999999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(4, 4)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111111'}})
 
     input.setSelectionRange(5, 5)
     TestUtils.Simulate.change(input, {target: {value: '1111111'}})
-    
+
     expect(input.selectionStart).to.be(5)
   })
 
   it('should move cursor to an available position if it is in the middle of user input #4', function() {
     var input = Input({mask: '999999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(5, 5)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111111'}})
 
     input.setSelectionRange(4, 4)
     TestUtils.Simulate.change(input, {target: {value: '11111'}})
-    
+
     expect(input.selectionStart).to.be(4)
   })
 
   it('should move cursor to an available position if it is in the middle of user input #5', function() {
     var input = Input({mask: '999-999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(2, 2)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111-111'}})
 
     input.setSelectionRange(3, 3)
     TestUtils.Simulate.change(input, {target: {value: '1121-111'}})
-    
+
     expect(input.selectionStart).to.be(3)
   })
 
   it('should move cursor to an available position if it is in the middle of user input #6', function() {
     var input = Input({mask: '999---999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(3, 3)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111---111'}})
 
     input.setSelectionRange(4, 4)
     TestUtils.Simulate.change(input, {target: {value: '1112---111'}})
-    
+
     expect(input.selectionStart).to.be(7)
   })
 
   it('should move cursor to an available position if it is in the middle of user input #7', function() {
     var input = Input({mask: '999---999'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(7, 7)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '111---111'}})
 
     input.setSelectionRange(6, 6)
     TestUtils.Simulate.change(input, {target: {value: '111---11'}})
-    
+
     expect(input.selectionStart).to.be(3)
   })
 
   it('should move cursor to an available position if it is in the middle of user input #8', function() {
     var input = Input({mask: '---999---999---'})
     TestUtils.Simulate.focus(input)
-    
+
     input.setSelectionRange(3, 3)
     TestUtils.Simulate.keyDown(input)
     TestUtils.Simulate.change(input, {target: {value: '---111---111---'}})
 
     input.setSelectionRange(2, 2)
     TestUtils.Simulate.change(input, {target: {value: '--111---11---'}})
-    
+
     expect(input.selectionStart).to.be(3)
   })
 })
